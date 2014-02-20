a24e56e180b886a92326f4b3cb5e72d2670b4088
Rakesh Sinha <sinhar@vmware.com>
2014-02-20 00:17:10 -0800

    Added devops related example projects.

---
+++ devops/artifact-managment/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>DevOps Artifact Management Hello World!i demo to Team.</h2>
+</body>
+</html>
---
+++ devops/pipeline-lib/src/main/java/com/vmware/vcac/devops/pipeline/PipelineWorkflowExecutor.java
@@ -0,0 +1,8 @@
+package com.vmware.vcac.devops.pipeline;
+
+public class PipelineWorkflowExecutor {
+
+    public static void main(String[] args) {
+        System.out.println("Executed DevOps Workflow...!!!!!!!!");
+    }
+}
